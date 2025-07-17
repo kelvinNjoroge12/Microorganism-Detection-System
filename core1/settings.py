0bd351374bcad5ef5d1f0ebf20b8b197f64a9b52
kelvinNjoroge12 <98796593+kelvinNjoroge12@users.noreply.github.com>
2025-07-17 15:20:44 +0300

    settings.py

--- core1/settings.py
+++ core1/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -39,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'predictions.middleware.ModelHealthCheckMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core1.urls'
